5fa0f7aa303ebf44a3ab0190aba8b3e08b3bab48
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-06-11 11:56:48 +0530

    Create Iterators.cpp

---
+++ Vector/Iterators.cpp
@@ -0,0 +1,42 @@
+// C++ program to illustrate the
+// iterators in vector
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void main() {
+	vector<int> g1{2,4,6,8,10};
+
+	cout << "Output of begin and end: ";
+	for (auto i = g1.begin(); i != g1.end(); ++i)
+		cout << *i << " ";
+	cout << "\nOutput of cbegin and cend: ";
+	for (auto i = g1.cbegin(); i != g1.cend(); ++i)
+		cout << *i << " ";
+	cout << "\nOutput of rbegin and rend: ";
+	for (auto ir = g1.rbegin(); ir != g1.rend(); ++ir)
+		cout << *ir << " ";
+	cout << "\nOutput of crbegin and crend : ";
+	for (auto ir = g1.crbegin(); ir != g1.crend(); ++ir)
+		cout << *ir << " ";
+
+}
+/*
+Output:
+Output of begin and end: 2 4 6 8 10
+Output of cbegin and cend: 2 4 6 8 10
+Output of rbegin and rend: 10 8 6 4 2
+Output of crbegin and crend : 10 8 6 4 2
+*/
+
+/*
+Iterators
+begin() – Returns an iterator pointing to the first element in the vector
+end() – Returns an iterator pointing to the theoretical element that follows the last element in the vector
+rbegin() – Returns a reverse iterator pointing to the last element in the vector (reverse beginning). It moves from last to first element
+rend() – Returns a reverse iterator pointing to the theoretical element preceding the first element in the vector (considered as reverse end)
+cbegin() – Returns a constant iterator pointing to the first element in the vector.
+cend() – Returns a constant iterator pointing to the theoretical element that follows the last element in the vector.
+crbegin() – Returns a constant reverse iterator pointing to the last element in the vector (reverse beginning). It moves from last to first element
+crend() – Returns a constant reverse iterator pointing to the theoretical element preceding the first element in the vector (considered as reverse end)
+*/
