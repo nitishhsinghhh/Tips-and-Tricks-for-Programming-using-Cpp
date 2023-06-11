19839d36bdd8fff20d397b9bee843aa83430a431
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-06-11 12:11:54 +0530

    Create Modifiers.cpp

---
+++ Vector/Modifiers.cpp
@@ -0,0 +1,109 @@
+// C++ program to illustrate the
+// element access in vector
+#include<iostream>
+#include <vector>
+using namespace std;
+
+void main() {
+	// Assign vector
+	vector<int> v;
+
+	// fill the vector with 10 five times
+	v.assign(5, 10);
+
+	cout << "The vector elements are: ";
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << " ";
+
+	// inserts 15 to the last position
+	v.push_back(15);
+	int n = v.size();
+	cout << "\nThe last element is: " << v[n - 1];
+
+	// removes last element
+	v.pop_back();
+
+	// prints the vector
+	cout << "\nThe vector elements are: ";
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << " ";
+
+	// inserts 5 at the beginning
+	v.insert(v.begin(), 5);
+
+	cout << "\nThe first element is: " << v[0];
+
+	// removes the first element
+	v.erase(v.begin());
+
+	cout << "\nThe first element is: " << v[0];
+
+	// inserts at the beginning
+	v.emplace(v.begin(), 5);
+	cout << "\nThe first element is: " << v[0];
+
+	// Inserts 20 at the end
+	v.emplace_back(20);
+	n = v.size();
+	cout << "\nThe last element is: " << v[n - 1];
+
+	// erases the vector
+	v.clear();
+	cout << "\nVector size after erase(): " << v.size();
+
+	// two vector to perform swap
+	vector<int> v1, v2;
+	v1.push_back(1);
+	v1.push_back(2);
+	v2.push_back(3);
+	v2.push_back(4);
+
+	cout << "\n\nVector 1: ";
+	for (int i = 0; i < v1.size(); i++)
+		cout << v1[i] << " ";
+
+	cout << "\nVector 2: ";
+	for (int i = 0; i < v2.size(); i++)
+		cout << v2[i] << " ";
+
+	// Swaps v1 and v2
+	v1.swap(v2);
+
+	cout << "\nAfter Swap \nVector 1: ";
+	for (int i = 0; i < v1.size(); i++)
+		cout << v1[i] << " ";
+
+	cout << "\nVector 2: ";
+	for (int i = 0; i < v2.size(); i++)
+		cout << v2[i] << " ";
+}
+
+/*
+Output:
+The vector elements are: 10 10 10 10 10
+The last element is: 15
+The vector elements are: 10 10 10 10 10
+The first element is: 5
+The first element is: 10
+The first element is: 5
+The last element is: 20
+Vector size after erase(): 0
+
+Vector 1: 1 2
+Vector 2: 3 4
+After Swap
+Vector 1: 3 4
+Vector 2: 1 2
+*/
+
+/*
+assign() – It assigns new value to the vector elements by replacing old ones
+push_back() – It push the elements into a vector from the back
+pop_back() – It is used to pop or remove elements from a vector from the back.
+insert() – It inserts new elements before the element at the specified position
+erase() – It is used to remove elements from a container from the specified position or range.
+swap() – It is used to swap the contents of one vector with another vector of same type. Sizes may differ.
+clear() – It is used to remove all the elements of the vector container
+emplace() – It extends the container by inserting new element at position
+emplace_back() – It is used to insert a new element into the vector container, the new element is added to the end of the vector
+*/
