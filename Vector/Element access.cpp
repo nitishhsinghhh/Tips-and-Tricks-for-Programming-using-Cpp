0d0edb90eedb3a3f3aa9a537f1f963cca5aae6aa
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-06-11 12:06:19 +0530

    Create Element access.cpp

---
+++ Vector/Element access.cpp
@@ -0,0 +1,36 @@
+// C++ program to illustrate the
+// element access in vector
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void main() {
+	vector<int> g1{ 2,4,6,8,10 };
+
+	cout << "\nReference operator [g] : g1[2] = " << g1[2];
+	cout << "\nat : g1.at(4) = " << g1.at(4);
+	cout << "\nfront() : g1.front() = " << g1.front();
+	cout << "\nback() : g1.back() = " << g1.back();
+
+	// pointer to the first element
+	int* pos = g1.data();
+
+	cout << "\nThe first element is " << *pos;
+}
+
+/*
+Output:
+Reference operator [g] : g1[2] = 6
+at : g1.at(4) = 10
+front() : g1.front() = 2
+back() : g1.back() = 10
+The first element is 2
+*/
+
+/*
+reference operator [g] – Returns a reference to the element at position ‘g’ in the vector
+at(g) – Returns a reference to the element at position ‘g’ in the vector
+front() – Returns a reference to the first element in the vector
+back() – Returns a reference to the last element in the vector
+data() – Returns a direct pointer to the memory array used internally by the vector to store its owned elements.
+*/
