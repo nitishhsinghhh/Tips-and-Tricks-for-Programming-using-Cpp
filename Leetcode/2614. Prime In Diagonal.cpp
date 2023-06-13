c01297578ee6fca0317e7c77fda5418243205d2e
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-06-13 21:56:25 +0530

    Update 2614. Prime In Diagonal.cpp

--- Leetcode/2614. Prime In Diagonal.cpp
+++ Leetcode/2614. Prime In Diagonal.cpp
@@ -18,3 +18,25 @@ public:
         return p;
     }
 };
+
+// Solution 2: Sieve of Eratosthenes
+
+bool sieve[4000001] =  { true, true }; 
+class Solution {
+    public:
+    void buildSieve() {
+        for (int p = 2; p * p < 400001; ++p)
+            if (!sieve[p])
+                for (int i = p * p; i < 400001; i += p)
+                    sieve[i] = true;
+    }
+    int diagonalPrime(vector<vector<int>>& n) {
+        if (!sieve[4])
+            buildSieve();
+        int p = 0;
+        for (int i = 0; i < n.size(); ++i)
+            p = max({p, !sieve[n[i][i]] * n[i][i], 
+                !sieve[n[i][n.size() - i - 1]] * n[i][n.size() - i - 1]});
+        return p;
+    }
+};
