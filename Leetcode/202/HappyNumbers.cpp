ac68f5453da1c339dbe1354577398e0602a87d2e
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-06-19 16:28:21 +0530

    Update HappyNumbers.cpp

--- Leetcode/202/HappyNumbers.cpp
+++ Leetcode/202/HappyNumbers.cpp
@@ -31,7 +31,7 @@ int main() {
     Solution oSolution;
     
     // Test cases
-    int testCases[] = {19, 7, 82, 68};
+    int testCases[] = {19, 7, 82, 68, 23, 44, 86, 91, 76, 39, 100, 33, 56, 8, 70, 14, 69, 95, 47, 13, 30, 98, 71, 50, 93, 80, 27, 59, 16, 84, 36, 60, 88, 67, 21, 79, 75, 51, 94, 62, 35, 48, 64, 12, 87, 40, 83, 52, 90, 74, 99, 18};
 
     for (int i = 0; i < sizeof(testCases) / sizeof(testCases[0]); i++) {
         std::cout << testCases[i] << " is a"
@@ -48,4 +48,51 @@ Output:
 7 is a happy number.
 82 is a happy number.
 68 is a happy number.
+23 is a happy number.
+44 is a happy number.
+86 is a happy number.
+91 is a happy number.
+76 is an unhappy number.
+39 is an unhappy number.
+100 is a happy number.
+33 is an unhappy number.
+56 is an unhappy number.
+8 is an unhappy number.
+70 is a happy number.
+14 is an unhappy number.
+69 is an unhappy number.
+95 is an unhappy number.
+47 is an unhappy number.
+13 is a happy number.
+30 is an unhappy number.
+98 is an unhappy number.
+71 is an unhappy number.
+50 is an unhappy number.
+93 is an unhappy number.
+80 is an unhappy number.
+27 is an unhappy number.
+59 is an unhappy number.
+16 is an unhappy number.
+84 is an unhappy number.
+36 is an unhappy number.
+60 is an unhappy number.
+88 is an unhappy number.
+67 is an unhappy number.
+21 is an unhappy number.
+79 is a happy number.
+75 is an unhappy number.
+51 is an unhappy number.
+94 is a happy number.
+62 is an unhappy number.
+35 is an unhappy number.
+48 is an unhappy number.
+64 is an unhappy number.
+12 is an unhappy number.
+87 is an unhappy number.
+40 is an unhappy number.
+83 is an unhappy number.
+52 is an unhappy number.
+90 is an unhappy number.
+74 is an unhappy number.
+99 is an unhappy number.
 */
