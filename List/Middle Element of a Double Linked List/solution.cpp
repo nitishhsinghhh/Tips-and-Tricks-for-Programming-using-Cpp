42b3f61a63a8069ba9579cdfeb096b8131524112
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-06-29 13:28:33 +0530

    Update solution.cpp

--- List/Middle Element of a Double Linked List/solution.cpp
+++ List/Middle Element of a Double Linked List/solution.cpp
@@ -2,71 +2,70 @@
 #include <list>
 using namespace std;
 
-void runTestCase(const list<int>& initialList) {
-    // Create a copy of the initial list
-    list<int> dblList = initialList;
+class Solution {
+public:
+	void deleteposElement(const list<int>& initialList, const int position) {
+		if (position == 0)
+			return;
 
-    // Find the middle iterator
-    auto middleIterator = dblList.begin();
-    advance(middleIterator, dblList.size() / 2);
+		list<int> dblList = initialList;
+		auto startiterator = dblList.begin();
+		if (position != 1)
+			advance(startiterator, position);
+		dblList.erase(startiterator);
 
-    // Delete the middle element
-    dblList.erase(middleIterator);
+		for (const auto& element : dblList)
+			cout << element << " ";
+		cout << endl;
+	}
+};
 
-    // Print the updated list
-    for (const auto& element : dblList)
-        cout << element << " ";
+void main() {
+	// Test Case 1: Deleting the middle element from an odd-sized list
+	Solution oSolution;
+	cout << "Test Case 1: Deleting the middle element from an odd-sized list" << endl;
+	list<int> testCase1 { 1, 2, 3, 4, 5 };
+	oSolution.deleteposElement(testCase1, testCase1.size() / 2); // Expected output: 1 2 4 5
 
-    cout << endl;
-}
+	// Test Case 2: Deleting the middle element from an even-sized list
+	cout << "Test Case 2: Deleting the middle element from an even-sized list" << endl;
+	list<int> testCase2 { 10, 20, 30, 40, 50, 60 };
+	oSolution.deleteposElement(testCase2, testCase1.size() / 2); // Expected output: 10 20 40 50 60
 
-int main() {
-    // Test Case 1: Deleting the middle element from an odd-sized list
-    cout << "Test Case 1: Deleting the middle element from an odd-sized list" << endl;
-    list<int> testCase1 = {1, 2, 3, 4, 5};
-    runTestCase(testCase1); // Expected output: 1 2 4 5
+	// Test Case 3: Deleting the only element from the list
+	cout << "Test Case 3: Deleting the only element from the list" << endl;
+	list<int> testCase3 { 100};
+	oSolution.deleteposElement(testCase3, testCase3.size()); // Expected output:
 
-    // Test Case 2: Deleting the middle element from an even-sized list
-    cout << "Test Case 2: Deleting the middle element from an even-sized list" << endl;
-    list<int> testCase2 = {10, 20, 30, 40, 50, 60};
-    runTestCase(testCase2); // Expected output: 10 20 40 50 60
+	// Test Case 4: Deleting the first element from the list
+	cout << "Test Case 4: Deleting the first element from the list" << endl;
+	list<int> testCase4 { 1, 2, 3, 4, 5 };
+	oSolution.deleteposElement(testCase4, 1); // Expected output: 2 3 4 5
 
-    // Test Case 3: Deleting the only element from the list
-    cout << "Test Case 3: Deleting the only element from the list" << endl;
-    list<int> testCase3 = {100};
-    runTestCase(testCase3); // Expected output:
+	// Test Case 5: Deleting the last element from the list
+	cout << "Test Case 5: Deleting the last element from the list" << endl;
+	list<int> testCase5 { 10, 20, 30, 40, 50 };
+	oSolution.deleteposElement(testCase5, testCase5.size() - 1); // Expected output: 10 20 30 40
 
-    // Test Case 4: Deleting the first element from the list
-    cout << "Test Case 4: Deleting the first element from the list" << endl;
-    list<int> testCase4 = {1, 2, 3, 4, 5};
-    runTestCase(testCase4); // Expected output: 2 3 4 5
-
-    // Test Case 5: Deleting the last element from the list
-    cout << "Test Case 5: Deleting the last element from the list" << endl;
-    list<int> testCase5 = {10, 20, 30, 40, 50};
-    runTestCase(testCase5); // Expected output: 10 20 30 40
-
-    // Test Case 6: Deleting from an empty list
-    cout << "Test Case 6: Deleting from an empty list" << endl;
-    list<int> testCase6 = {};
-    runTestCase(testCase6); // Expected output:
-
-    return 0;
+	// Test Case 6: Deleting from an empty list
+	cout << "Test Case 6: Deleting from an empty list" << endl;
+	list<int> testCase6 {};
+	oSolution.deleteposElement(testCase6, 0); // Expected output:
+    
 }
 
 /*
 Output:
 Test Case 1: Deleting the middle element from an odd-sized list
-1 2 4 5 
+1 2 4 5
 Test Case 2: Deleting the middle element from an even-sized list
-10 20 30 50 60 
+10 20 40 50 60
 Test Case 3: Deleting the only element from the list
 
 Test Case 4: Deleting the first element from the list
-1 2 4 5 
+2 3 4 5
 Test Case 5: Deleting the last element from the list
-10 20 40 50 
+10 20 30 40
 Test Case 6: Deleting from an empty list
-double free or corruption (out)
-Aborted
+
 */
