b9fe86eb7dd3b5ec898a7a2902de040227b2794d
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-07-01 08:17:13 +0530

    Create vectorTemplate.cpp

---
+++ Queue/Implementation/vectorTemplate.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+template<typename T>
+class Queue {
+private:
+   vector<T> data;
+public: 
+   bool isEmpty(){
+       return data.empty();
+   }
+   
+   void push(T element){
+       data.push_back(element);
+   }
+   
+   void pop(){
+       if(!isEmpty())
+          data.erase(data.begin());
+   }
+   
+   T front(){
+       return data.front();
+   }
+};
+
+int main() {
+    Queue<int> queue;
+
+    queue.push(1);
+    queue.push(2);
+    queue.push(3);
+
+    std::cout << "Front: " << queue.front() << std::endl;
+
+    queue.pop();
+    return 0;
+}
