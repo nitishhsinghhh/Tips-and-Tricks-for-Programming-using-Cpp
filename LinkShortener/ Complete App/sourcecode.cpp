42566862529a43cb8efd203d5dd868eb36ca4826
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-07-30 14:51:28 +0530

    Create  sourcecode.cpp

---
+++ LinkShortener/ Complete App/sourcecode.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <random>
+
+using namespace std;
+
+class LinkShortener {
+private:
+	unordered_map<string, string> links;
+	string base_url;
+
+	string generateRandomCode(int length) {
+		const string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		random_device rd;
+		mt19937 generator(rd());
+		uniform_int_distribution<int> distribution(0, characters.size() - 1);
+		string code;
+		for (int i = 0; i < length; ++i)
+			code += characters[distribution(generator)];
+		return code;
+	}
+
+public:
+	LinkShortener(const string& baseUrl) : base_url(baseUrl) {}
+
+	string shortenLink(const string& originalLink) {
+		string shortCode;
+
+		do {
+			shortCode = generateRandomCode(6);
+		} while (links.find(base_url + shortCode) != links.end());
+
+		string shortenedLink = base_url + shortCode;
+		links[shortenedLink] = originalLink;
+
+		return shortenedLink;
+	}
+
+	string getOriginalLink(const string& shortenedLink) {
+		if (links.find(shortenedLink) != links.end())
+			return links[shortenedLink];
+
+		return ""; // Return empty string if the shortened link doesn't exist
+	}
+};
+
+int main() {
+	cout << "Base URL: ";
+	string baseUrl = "";
+	cin >> baseUrl;
+	cout << endl;
+
+	LinkShortener shortener(baseUrl);
+
+	string originalLink;
+	cout << "URL Link to be Shotened: ";
+	cin >> originalLink;
+	cout << endl;
+
+	string shortenedLink = shortener.shortenLink(originalLink);
+	cout << "Shortened link: " << shortenedLink << endl;
+
+	string retrievedLink = shortener.getOriginalLink(shortenedLink);
+	if (!retrievedLink.empty())
+		cout << "Original link: " << retrievedLink << endl;
+	else
+		cout << "Invalid or non-existent shortened link." << endl;
+
+	return 0;
+}
