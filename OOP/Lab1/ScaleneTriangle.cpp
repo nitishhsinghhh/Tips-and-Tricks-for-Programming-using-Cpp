68236c540e0e824aedf4b8f0725d55d89f9af254
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-08-01 17:11:32 +0530

    Create ScaleneTriangle.cpp

---
+++ OOP/Lab1/ScaleneTriangle.cpp
@@ -0,0 +1,36 @@
+// C++ program to Calculate Area of Scalene Triangle
+
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main() {
+	cout << "\t\tC++ program to Calculate Area of Scalene Triangle. \n";
+	float a, b, c, S, Area;
+	cout << "Please enter the values for the first side of Triangle: ";
+	cin >> a;
+	cout << "Please enter the values for the second side of Triangle: ";
+	cin >> b;
+	cout << "Please enter the values for the third side of Triangle: ";
+	cin >> c;
+
+	if (a > 0 && b > 0 && c > 0) {
+		if (c < a + b && a < b + c && b < a + c) {
+			S = (a + b + c) / 2;
+			Area = sqrt(S * (S - a) * (S - b) * (S - c));
+			cout << "The Area of Triangle is " << Area << endl;
+			return 0;
+		}
+		else {
+			cout << "This triangle is wrong try another values." << endl;
+			return 0;
+		}
+	}
+	else
+	{
+		cout << "The sides of Triangle cannot be negative numbers!" << endl;
+	}
+	system("pause");
+	return 0;
+}
