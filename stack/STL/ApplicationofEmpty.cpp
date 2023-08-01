e3283a349374d2965419a59e207dd4f6ead94050
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-08-01 12:38:14 +0530

    Create ApplicationofEmpty.cpp

---
+++ stack/STL/ApplicationofEmpty.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<stack>
+
+void main() {
+	int sum = 0;
+	std::stack<int> myStack;
+	myStack.push(1);
+	myStack.push(8);
+	myStack.push(3);
+	myStack.push(6);
+	myStack.push(2);
+
+	// Stack becomes 1, 8, 3, 6, 2
+
+	while (!myStack.empty())
+	{
+		sum += myStack.top();
+		myStack.pop();
+	}
+	std::cout << sum;
+}
