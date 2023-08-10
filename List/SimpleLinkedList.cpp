3d86e76d2569315e1fb7cf94dea56f8ec38e2f6a
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-08-10 11:54:59 +0530

    Update SimpleLinkedList.cpp

--- List/SimpleLinkedList.cpp
+++ List/SimpleLinkedList.cpp
@@ -34,3 +34,39 @@ int main() {
     return 0;
 }
 // 1 2 3
+
+
+// Program to construct and Access the elements of a Linked List
+
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode(): val(0), next(nullptr) {}
+	ListNode(int x): val(x), next(nullptr) {}
+	ListNode(int x, ListNode *next): val(x), next(next) {}
+};
+
+int main() {
+	ListNode *head = new ListNode(1);
+	head->next = new ListNode(2);
+	head->next->next = new ListNode(3);
+	head->next->next->next = new ListNode(4);
+	head->next->next->next->next = new ListNode(5);
+
+	// Accessing linked list elements
+	ListNode *current = head;
+	cout << "Output: ";
+	while (current != nullptr) {
+		std::cout << current->val << " ";
+		current = current->next;
+	}
+	cout << endl;
+
+	system("pause");
+}
+/*
+Output: 1 2 3 4 5
+*/
