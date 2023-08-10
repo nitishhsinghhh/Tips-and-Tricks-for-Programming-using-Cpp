f2523e1b195a639b60c90eb34619b9c3b0e394a3
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-08-10 11:50:24 +0530

    Update creatingInitilizing.cpp

--- List/creatingInitilizing.cpp
+++ List/creatingInitilizing.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
-#include <list>
-using namespace std;
+// Program to show elements of a list
+
+#include<iostream>
+#include<list>
 
 void main() {
- list<int> myList{ 1, 2, 3, 4, 5 };
- // Accessing elements using a range-based for loop
- for (auto element : myList) 
-  cout << element << " ";
-}
+	std::list<int> myList { 1, 2, 3, 4, 5 };
 
+	// Accessing the range based for loop
+	std::cout << "Output: ";
+	for (int element : myList)
+		std::cout << element << " ";
+	std::cout << std::endl;
+	system("pause");
+}
 // output: 1 2 3 4 5.
