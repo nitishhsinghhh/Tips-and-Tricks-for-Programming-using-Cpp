a680ec7e3239b70eca51ce8bb2c9e0660b4216f9
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-08-10 15:37:57 +0530

    Update pattern2.cpp

--- OOP/Lab4 /pattern2.cpp
+++ OOP/Lab4 /pattern2.cpp
@@ -6,10 +6,10 @@ using namespace std;
 class Solution {
 public:
 	void CreatePattern(int num) {
-		for (int i = 1; i <= num; i +=2) {
-			for (int j = num - 2; j >= i; j -=2)
+		for (int i = 1; i <= num; i += 2) {
+			for (int j = num - 2; j >= i; j -= 2)
 				cout << " ";
-			for (int j = 1; j <= 1; j++)
+			for (int j = 1; j <= i; j++)
 				cout << "*";
 			cout << endl;
 		}
@@ -30,37 +30,38 @@ void main() {
 	system("pause");
 }
 
+
 /*
 Output:
 
   *
- *
-*
+ ***
+*****
 
    *
-  *
- *
-*
+  ***
+ *****
+*******
 
     *
-   *
-  *
- *
-*
+   ***
+  *****
+ *******
+*********
 
      *
-    *
-   *
-  *
- *
-*
+    ***
+   *****
+  *******
+ *********
+***********
 
       *
-     *
-    *
-   *
-  *
- *
-*
+     ***
+    *****
+   *******
+  *********
+ ***********
+*************
 
 */
