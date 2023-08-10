b81e2529faa58956fc10cf1b50d969597f8f1082
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-08-10 16:36:56 +0530

    Create source1.cpp

---
+++ OOP/Lab5/source1.cpp
@@ -0,0 +1,13 @@
+// Program to swap two values using functions
+
+#include<iostream>
+using namespace std;
+
+
+void main() {
+	int a, b;
+	cout << "Enter two numbers: ";
+	cin >> a >> b;
+	swap(a, b);
+	cout << a << " & " << b << endl;
+}
