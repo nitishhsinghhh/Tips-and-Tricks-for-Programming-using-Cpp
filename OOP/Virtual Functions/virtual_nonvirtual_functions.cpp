0896a4bcfc2328a4e4711ca0140a8be87ac0d38e
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-08-13 18:14:25 +0530

    Create virtual_nonvirtual_functions.cpp

---
+++ OOP/Virtual Functions/virtual_nonvirtual_functions.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+   virtual void NameOf();   // Virtual function.
+   void InvokingClass();   // Nonvirtual function.
+};
+
+// Implement the two functions.
+void Base::NameOf() {
+   cout << "Base::NameOf\n";
+}
+
+void Base::InvokingClass() {
+   cout << "Invoked by Base\n";
+}
+
+class Derived : public Base {
+public:
+   void NameOf();   // Virtual function.
+   void InvokingClass();   // Nonvirtual function.
+};
+
+// Implement the two functions.
+void Derived::NameOf() {
+   cout << "Derived::NameOf\n";
+}
+
+void Derived::InvokingClass() {
+   cout << "Invoked by Derived\n";
+}
+
+int main() {
+   // Declare an object of type Derived.
+   Derived aDerived;
+
+   // Declare two pointers, one of type Derived * and the other
+   //  of type Base *, and initialize them to point to aDerived.
+   Derived *pDerived = &aDerived;
+   Base    *pBase    = &aDerived;
+
+   // Call the functions.
+   pBase->NameOf();           // Call virtual function.
+   pBase->InvokingClass();    // Call nonvirtual function.
+   pDerived->NameOf();        // Call virtual function.
+   pDerived->InvokingClass(); // Call nonvirtual function.
+}
+/*
+Output:
+Derived::NameOf
+Invoked by Base
+Derived::NameOf
+Invoked by Derived
+*/
