b1dbc82592a07e2a18efb268e106829d96acfb9e
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-09-05 16:25:24 +0530

    Update and rename modifyValues.cpp to  modifyValues.cpp

---
+++ PassBYValueReferenceAddress/ modifyValues.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+// Function to modify a variable passed by value
+void modifyByValue(int num) {
+	num = 42;
+}
+
+// Function to modify a variable passed by reference
+void modifyByReference(int& num) {
+	num = 42;
+}
+
+void modifyByAddress(int *num) {
+	*num = 42;
+}
+
+int main() {
+	int number = 7;
+	int a = 5;
+
+	// Pass 'number' by value
+	modifyByValue(number);
+	std::cout << "Value after modifyByValue: " << number << std::endl; // Output: 7
+
+	// Pass 'number' by reference
+	modifyByReference(number);
+	std::cout << "Value after modifyByReference: " << number << std::endl; // Output: 42
+
+	modifyByAddress(&a);
+	std::cout << "Value after modifyByAddress: " << a << std::endl; // Output: 42
+
+	return 0;
+}
