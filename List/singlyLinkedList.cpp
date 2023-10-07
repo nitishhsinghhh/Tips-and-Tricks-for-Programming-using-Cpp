063c599ebf2cbafc3b9cd8b2bb7d31fbd65081e8
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-10-07 15:19:14 +0530

    Update singlyLinkedList.cpp

--- List/singlyLinkedList.cpp
+++ List/singlyLinkedList.cpp
@@ -1,72 +1,23 @@
 #include <iostream>
 
+// Definition for singly-linked list.
 struct Node {
-    int data;
-    Node* next;
+ int data;
+ Node* next;
+ Node() : data(0), next(nullptr) {}
+ Node(int x) : data(x), next(nullptr) {}
+ Node(int x, Node *next) : data(x), next(next) {}
 };
 
-int main() {
-    Node* head = nullptr;
+void main() {
+ Node* head = new Node(1, new Node(2, new Node(3)));
 
-    // Adding elements to the linked list
-    Node* node1 = new Node();
-    node1->data = 1;
-    node1->next = nullptr;
-    head = node1;
+ while (head != nullptr) {
+  std::cout << head->data << " ";
+  head = head->next;
+ }
+ std::cout << std::endl;
 
-    Node* node2 = new Node();
-    node2->data = 2;
-    node2->next = nullptr;
-    node1->next = node2;
-
-    Node* node3 = new Node();
-    node3->data = 3;
-    node3->next = nullptr;
-    node2->next = node3;
-
-    // Accessing linked list elements
-    Node* current = head;
-    while (current != nullptr) {
-        std::cout << current->data << " ";
-        current = current->next;
-    }
-
-    return 0;
+ system("pause");
 }
 // 1 2 3
-
-
-// Program to construct and Access the elements of a Linked List
-
-#include <iostream>
-using namespace std;
-
-struct ListNode {
-	int val;
-	ListNode *next;
-	ListNode(): val(0), next(nullptr) {}
-	ListNode(int x): val(x), next(nullptr) {}
-	ListNode(int x, ListNode *next): val(x), next(next) {}
-};
-
-int main() {
-	ListNode *head = new ListNode(1);
-	head->next = new ListNode(2);
-	head->next->next = new ListNode(3);
-	head->next->next->next = new ListNode(4);
-	head->next->next->next->next = new ListNode(5);
-
-	// Accessing linked list elements
-	ListNode *current = head;
-	cout << "Output: ";
-	while (current != nullptr) {
-		std::cout << current->val << " ";
-		current = current->next;
-	}
-	cout << endl;
-
-	system("pause");
-}
-/*
-Output: 1 2 3 4 5
-*/
