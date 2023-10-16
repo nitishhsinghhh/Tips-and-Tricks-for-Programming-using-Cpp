af228cc37494131c5fb00f54a9295f9cd935b6ff
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-10-16 17:11:03 +0530

    Update LAB1.cpp

--- sort/Sorting Methods/Generalised Couting Sort /LAB1.cpp
+++ sort/Sorting Methods/Generalised Couting Sort /LAB1.cpp
@@ -37,3 +37,10 @@ int main() {
     cout << endl;
     return 0;
 }
+
+/*
+Output:
+Input array: {2 5 3 0 2 3 0 3 }
+Sorted array (ascending): {0 0 2 2 3 3 3 5 }
+Sorted array (descending): {5 3 3 3 2 2 0 0 }
+*/
