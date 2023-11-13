c462f4234131f86a325f31c00893ef21c4cbecd0
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-11-13 19:43:14 +0530

    Update performingASCIIcalculations.cpp

--- ASCII/performingASCIIcalculations.cpp
+++ ASCII/performingASCIIcalculations.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
 
-void main() {
- char char1 = '3';
- char char2 = '7';
- int num1 = char1 - '0'; // converting character to integer
- int num2 = char2 - '0'; // converting character to integer
- int sum = num1 + num2;
- cout << "The sum of " << char1 << " and " << char2 << " is " << sum << endl;
+// Single Responsibility Principle: This class is responsible for adding two characters together
+class CharAdder {
+public:
+	char char1, char2;
 
+	CharAdder(char c1, char c2) : char1(c1), char2(c2) {}
+	int add() {
+		int num1 = char1 - '0';
+		int num2 = char2 - '0';
+		return num1 + num2;
+	}
+};
+
+int main() {
+	CharAdder adder('3', '7');
+	int sum = adder.add();
+	// Single Responsibility Principle: This line is responsible for outputting the result to the console
+	cout << "The sum of " << adder.char1 << " and " << adder.char2 << " is " << sum << endl;
+	system("pause");
+	return 0;
 }
 
 // Output: The sum of 3 and 7 is 10
