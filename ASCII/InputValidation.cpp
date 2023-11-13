3c8cf209770b3c880ad2fb920d8570def7b7f32d
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-11-13 19:49:09 +0530

    Update InputValidation.cpp

--- ASCII/InputValidation.cpp
+++ ASCII/InputValidation.cpp
@@ -2,15 +2,43 @@
 #include <string>
 using namespace std;
 
-void main() {
+// Single Responsibility Principle: This class is responsible for validating user input
+class InputValidator {
+public:
+	InputValidator(char c) : input_char(c) {}
+	bool validate() {
+		int input_num = input_char - '0';
+		return input_num >= 1 && input_num <= 10;
+	}
+	int getNum() {
+		return input_char - '0';
+	}
+private:
 	char input_char;
-	int input_num;
-	cout << "Enter a number between 1 and 10: ";
-	cin >> input_char;
-	input_num = input_char - '0'; // Converting character to integer
-	input_num >= 1 && input_num <= 10 ? cout << "Valid input: " << input_num << endl
-		: cout << "Invalid input: " << input_char << endl;
+};
 
+// Single Responsibility Principle: This class is responsible for handling user input and output
+class InputHandler {
+public:
+	InputHandler() {}
+	void handle() {
+		char input_char;
+		cout << "Enter a number between 1 and 10: ";
+		cin >> input_char;
+
+		// Create an instance of InputValidator to validate the input
+		InputValidator validator(input_char);
+		if (validator.validate()) 
+			cout << "Valid input: " << validator.getNum() << endl;
+		else 
+			cout << "Invalid input: " << input_char << endl;
+	}
+};
+
+int main() {
+	// Create an instance of InputHandler to handle user input and output
+	InputHandler handler;
+	handler.handle();
 	system("pause");
 }
 
