ea45f1127df9d0bff269792448ba0ffa8de6e9e3
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-11-13 19:34:12 +0530

    Update InputValidation.cpp

--- ASCII/InputValidation.cpp
+++ ASCII/InputValidation.cpp
@@ -3,14 +3,15 @@
 using namespace std;
 
 void main() {
- char input_char;
- int input_num;
- cout << "Enter a number between 1 and 10: ";
- cin >> input_char;
- input_num = input_char - '0'; // Converting character to integer
- input_num >= 1 && input_num <= 10 ? cout << "Valid input: " << input_num << endl 
-  : cout << "Invalid input: " << input_char << endl;
+	char input_char;
+	int input_num;
+	cout << "Enter a number between 1 and 10: ";
+	cin >> input_char;
+	input_num = input_char - '0'; // Converting character to integer
+	input_num >= 1 && input_num <= 10 ? cout << "Valid input: " << input_num << endl
+		: cout << "Invalid input: " << input_char << endl;
 
+	system("pause");
 }
 
 /*
