2327b6b372ee400566856c03126ffda529cd35f5
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-12-11 21:37:58 +0530

    Update and rename Usingqueue.cpp to UsingDeque.cpp

---
+++ stack/implementation/UsingDeque.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <deque>
+
+class DequeStack {
+private:
+    std::deque<int> stack;
+public:
+    void push(int data) {
+        stack.push_back(data);
+    }
+    void pop() {
+        if (!stack.empty()) {
+            stack.pop_back();
+        }
+    }
+    int peek() {
+        if (!stack.empty()) {
+            return stack.back();
+        }
+        return -1; // Or throw an exception
+    }
+    bool isEmpty() {
+        return stack.empty();
+    }
+};
+
+int main() {
+    DequeStack stack;
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    std::cout << "Peek: " << stack.peek() << std::endl;
+
+    stack.pop();
+    std::cout << "Peek after pop: " << stack.peek() << std::endl;
+
+    std::cout << "Is empty? " << (stack.isEmpty() ? "Yes" : "No") << std::endl;
+
+    return 0;
+}
