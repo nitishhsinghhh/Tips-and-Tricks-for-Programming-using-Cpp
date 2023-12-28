ef6751e5680001dcd891f2c8840ac68790e89470
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-12-28 09:51:09 +0530

    Update UsingDeque.cpp

--- stack/implementation/UsingDeque.cpp
+++ stack/implementation/UsingDeque.cpp
@@ -3,39 +3,41 @@
 
 class DequeStack {
 private:
-    std::deque<int> stack;
+	std::deque<int> stack;
 public:
-    void push(int data) {
-        stack.push_back(data);
-    }
-    void pop() {
-        if (!stack.empty()) {
-            stack.pop_back();
-        }
-    }
-    int peek() {
-        if (!stack.empty()) {
-            return stack.back();
-        }
-        return -1; // Or throw an exception
-    }
-    bool isEmpty() {
-        return stack.empty();
-    }
+	void push(int data) {
+		stack.push_back(data);
+	}
+	void pop() {
+		if (!stack.empty()) {
+			stack.pop_back();
+		}
+	}
+	int peek() {
+		if (!stack.empty()) {
+			return stack.back();
+		}
+		return -1; // Or throw an exception
+	}
+	bool isEmpty() {
+		return stack.empty();
+	}
 };
 
-int main() {
-    DequeStack stack;
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
-
-    std::cout << "Peek: " << stack.peek() << std::endl;
-
-    stack.pop();
-    std::cout << "Peek after pop: " << stack.peek() << std::endl;
-
-    std::cout << "Is empty? " << (stack.isEmpty() ? "Yes" : "No") << std::endl;
-
-    return 0;
+void main() {
+	DequeStack stack;
+	stack.push(10);
+	stack.push(20);
+	stack.push(30);
+	std::cout << "Peek: " << stack.peek() << std::endl;
+	stack.pop();
+	std::cout << "Peek after pop: " << stack.peek() << std::endl;
+	std::cout << "Is empty? " << (stack.isEmpty() ? "Yes" : "No") << std::endl;
+	system("pause");
 }
+/*
+Top element: 50
+Top element after pop: 40
+Stack size: 2
+Press any key to continue . . .
+*/
