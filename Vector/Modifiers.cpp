316403016f6e79b4a2915d570e7b7aa7de114a56
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-12-28 21:29:44 +0530

    Update Modifiers.cpp

--- Vector/Modifiers.cpp
+++ Vector/Modifiers.cpp
@@ -2,84 +2,85 @@
 // element access in vector
 #include<iostream>
 #include <vector>
-using namespace std;
 
 void main() {
 	// Assign vector
-	vector<int> v;
+	std::vector<int> v;
 
 	// fill the vector with 10 five times
 	v.assign(5, 10);
 
-	cout << "The vector elements are: ";
+	std::cout << "The vector elements are: ";
 	for (int i = 0; i < v.size(); i++)
-		cout << v[i] << " ";
+		std::cout << v[i] << " ";
 
 	// inserts 15 to the last position
 	v.push_back(15);
 	int n = v.size();
-	cout << "\nThe last element is: " << v[n - 1];
+	std::cout << "\nThe last element is: " << v[n - 1];
 
 	// removes last element
 	v.pop_back();
 
 	// prints the vector
-	cout << "\nThe vector elements are: ";
+	std::cout << "\nThe vector elements are: ";
 	for (int i = 0; i < v.size(); i++)
-		cout << v[i] << " ";
+		std::cout << v[i] << " ";
 
 	// inserts 5 at the beginning
 	v.insert(v.begin(), 5);
 
-	cout << "\nThe first element is: " << v[0];
+	std::cout << "\nThe first element is: " << v[0];
 
 	// removes the first element
 	v.erase(v.begin());
-
-	cout << "\nThe first element is: " << v[0];
+	std::cout << "\nThe first element is: " << v[0];
 
 	// inserts at the beginning
 	v.emplace(v.begin(), 5);
-	cout << "\nThe first element is: " << v[0];
+	std::cout << "\nThe first element is: " << v[0];
 
 	// Inserts 20 at the end
 	v.emplace_back(20);
 	n = v.size();
-	cout << "\nThe last element is: " << v[n - 1];
+	std::cout << "\nThe last element is: " << v[n - 1];
 
 	// erases the vector
 	v.clear();
-	cout << "\nVector size after erase(): " << v.size();
+	std::cout << "\nVector size after erase(): " << v.size();
 
 	// two vector to perform swap
-	vector<int> v1, v2;
+	std::vector<int> v1, v2;
 	v1.push_back(1);
 	v1.push_back(2);
 	v2.push_back(3);
 	v2.push_back(4);
 
-	cout << "\n\nVector 1: ";
+	std::cout << "\n\nVector 1: ";
 	for (int i = 0; i < v1.size(); i++)
-		cout << v1[i] << " ";
+		std::cout << v1[i] << " ";
 
-	cout << "\nVector 2: ";
+	std::cout << "\nVector 2: ";
 	for (int i = 0; i < v2.size(); i++)
-		cout << v2[i] << " ";
+		std::cout << v2[i] << " ";
 
 	// Swaps v1 and v2
 	v1.swap(v2);
 
-	cout << "\nAfter Swap \nVector 1: ";
+	std::cout << "\nAfter Swap \nVector 1: ";
 	for (int i = 0; i < v1.size(); i++)
-		cout << v1[i] << " ";
+		std::cout << v1[i] << " ";
 
-	cout << "\nVector 2: ";
+	std::cout << "\nVector 2: ";
 	for (int i = 0; i < v2.size(); i++)
-		cout << v2[i] << " ";
+		std::cout << v2[i] << " ";
+
+	std::cout << std::endl;
+	system("pause");
 }
 
+
 /*
-Output:
 The vector elements are: 10 10 10 10 10
 The last element is: 15
 The vector elements are: 10 10 10 10 10
@@ -94,6 +95,7 @@ Vector 2: 3 4
 After Swap
 Vector 1: 3 4
 Vector 2: 1 2
+Press any key to continue . . .
 */
 
 /*
