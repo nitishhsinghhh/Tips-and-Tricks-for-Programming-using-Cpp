0597f4493157afce3db91d5d521d0d65a99b1cad
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-12-28 21:09:01 +0530

    Update Iterators.cpp

--- Vector/Iterators.cpp
+++ Vector/Iterators.cpp
@@ -2,31 +2,31 @@
 // iterators in vector
 #include <iostream>
 #include <vector>
-using namespace std;
 
 void main() {
-	vector<int> g1{2,4,6,8,10};
+	std::vector<int> g1{ 2,4,6,8,10 };
 
-	cout << "Output of begin and end: ";
+	std::cout << "Output of begin and end: ";
 	for (auto i = g1.begin(); i != g1.end(); ++i)
-		cout << *i << " ";
-	cout << "\nOutput of cbegin and cend: ";
+		std::cout << *i << " ";
+	std::cout << "\nOutput of cbegin and cend: ";
 	for (auto i = g1.cbegin(); i != g1.cend(); ++i)
-		cout << *i << " ";
-	cout << "\nOutput of rbegin and rend: ";
+		std::cout << *i << " ";
+	std::cout << "\nOutput of rbegin and rend: ";
 	for (auto ir = g1.rbegin(); ir != g1.rend(); ++ir)
-		cout << *ir << " ";
-	cout << "\nOutput of crbegin and crend : ";
+		std::cout << *ir << " ";
+	std::cout << "\nOutput of crbegin and crend : ";
 	for (auto ir = g1.crbegin(); ir != g1.crend(); ++ir)
-		cout << *ir << " ";
-
+		std::cout << *ir << " ";
+	std::cout << std::endl;
+	system("pause");
 }
 /*
-Output:
 Output of begin and end: 2 4 6 8 10
 Output of cbegin and cend: 2 4 6 8 10
 Output of rbegin and rend: 10 8 6 4 2
 Output of crbegin and crend : 10 8 6 4 2
+Press any key to continue . . .
 */
 
 /*
