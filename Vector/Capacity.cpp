f52fc3b5bc1b31599f4fdef93f9d44df9e749b47
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2023-12-28 21:03:09 +0530

    Update Capacity.cpp

--- Vector/Capacity.cpp
+++ Vector/Capacity.cpp
@@ -3,43 +3,41 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
 void main() {
-	vector<int> g1{ 2,4,6,8,10 };
+	std::vector<int> g1{ 2,4,6,8,10 };
 
-	cout << "Size : " << g1.size();
-	cout << "\nCapacity : " << g1.capacity();
-	cout << "\nMax_Size : " << g1.max_size();
+	std::cout << "Size : " << g1.size();
+	std::cout << "\nCapacity : " << g1.capacity();
+	std::cout << "\nMax_Size : " << g1.max_size();
 
 	// resizes the vector size to 4
 	g1.resize(4);
 
 	// prints the vector size after resize()
-	cout << "\nSize : " << g1.size();
+	std::cout << "\nSize : " << g1.size();
 
 	// checks if the vector is empty or not
 	if (g1.empty() == false)
-		cout << "\nVector is not empty";
+		std::cout << "\nVector is not empty";
 	else
-		cout << "\nVector is empty";
+		std::cout << "\nVector is empty";
 
 	// Shrinks the vector
 	g1.shrink_to_fit();
-	cout << "\nVector elements are: ";
+	std::cout << "\nVector elements are: ";
 	for (auto it = g1.begin(); it != g1.end(); it++)
-		cout << *it << " ";
-
+		std::cout << *it << " ";
+	std::cout << std::endl;
+	system("pause");
 }
-
 /*
-Output:
 Size : 5
 Capacity : 5
 Max_Size : 1073741823
 Size : 4
 Vector is not empty
 Vector elements are: 2 4 6 8
+Press any key to continue . . .
 */
 
 /*
