0c74b34e4bbf8d114b56491d54af737b327c9b53
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-01-02 11:09:24 +0530

    Update Element access.cpp

--- Vector/Element access.cpp
+++ Vector/Element access.cpp
@@ -29,8 +29,8 @@ Press any key to continue . . .
 */
 
 /*
-reference operator [g] – Returns a reference to the element at position ‘g’ in the vector
 at(g) – Returns a reference to the element at position ‘g’ in the vector
+operator [g] – Returns a reference to the element at position ‘g’ in the vector
 front() – Returns a reference to the first element in the vector
 back() – Returns a reference to the last element in the vector
 data() – Returns a direct pointer to the memory array used internally by the vector to store its owned elements.
