d660a3c5efe11515d7165c7fee0524267e890bb1
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-01-02 11:06:06 +0530

    Update Iterators.cpp

--- Vector/Iterators.cpp
+++ Vector/Iterators.cpp
@@ -31,12 +31,8 @@ Press any key to continue . . .
 
 /*
 Iterators
-begin() – Returns an iterator pointing to the first element in the vector
-end() – Returns an iterator pointing to the theoretical element that follows the last element in the vector
-rbegin() – Returns a reverse iterator pointing to the last element in the vector (reverse beginning). It moves from last to first element
-rend() – Returns a reverse iterator pointing to the theoretical element preceding the first element in the vector (considered as reverse end)
-cbegin() – Returns a constant iterator pointing to the first element in the vector.
-cend() – Returns a constant iterator pointing to the theoretical element that follows the last element in the vector.
-crbegin() – Returns a constant reverse iterator pointing to the last element in the vector (reverse beginning). It moves from last to first element
-crend() – Returns a constant reverse iterator pointing to the theoretical element preceding the first element in the vector (considered as reverse end)
+begin() / cbegin() (C++11)– Returns an iterator pointing to the first element in the vector
+end() / cend() (C++11)– Returns an iterator pointing to the theoretical element that follows the last element in the vector
+rbegin() / crbegin() (C++11)– Returns a reverse iterator pointing to the last element in the vector (reverse beginning). It moves from last to first element
+rend() / crend() (C++11)– Returns a reverse iterator pointing to the theoretical element preceding the first element in the vector (considered as reverse end)
 */
