c5d0ade168bb0325c6e80d94581ab3be89ff9255
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-01-02 11:08:30 +0530

    Update Capacity.cpp

--- Vector/Capacity.cpp
+++ Vector/Capacity.cpp
@@ -10,9 +10,6 @@ void main() {
 	std::cout << "\nCapacity : " << g1.capacity();
 	std::cout << "\nMax_Size : " << g1.max_size();
 
-	// resizes the vector size to 4
-	g1.resize(4);
-
 	// prints the vector size after resize()
 	std::cout << "\nSize : " << g1.size();
 
@@ -41,11 +38,10 @@ Press any key to continue . . .
 */
 
 /*
+empty() – Returns whether the container is empty.
 size() – Returns the number of elements in the vector.
 max_size() – Returns the maximum number of elements that the vector can hold.
-capacity() – Returns the size of the storage space currently allocated to the vector expressed as number of elements.
-resize(n) – Resizes the container so that it contains ‘n’ elements.
-empty() – Returns whether the container is empty.
-shrink_to_fit() – Reduces the capacity of the container to fit its size and destroys all elements beyond the capacity.
 reserve() – Requests that the vector capacity be at least enough to contain n elements.
+capacity() – Returns the size of the storage space currently allocated to the vector expressed as number of elements.
+shrink_to_fit() – Reduces the capacity of the container to fit its size and destroys all elements beyond the capacity.
 */
