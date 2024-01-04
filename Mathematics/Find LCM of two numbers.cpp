24e834c534363d764a8e6dc13899f27a7a0403bf
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-04 20:29:10 +0530

    Finding LCM of two numbers using the concept of GCD

---
+++ Mathematics/Find LCM of two numbers.cpp
@@ -0,0 +1,54 @@
+// Write a program that calculates the LCM of two numbers. Take the numbers as input.
+#include <iostream>
+class LCM{
+    private:
+        int a, b;
+        //calculate gcd using Euclidean Algorithm
+        int gcd(int a, int b){
+          if(a==0) return b;
+          else if(b==0) return a;
+          if(a==b) return b;
+          if(a>b) gcd(a%b,b);
+          else if(b>a) gcd(a,b%a);
+        }
+        //calculate lcm where [LCM = (a*b)/gcd(a,b)]
+        long long calcLCM(int x, int y)
+        {
+          long long mul = x;
+          mul *= y;
+          long long res = mul / gcd(x,y);
+          return res;
+        }
+
+    public:
+        //constructor
+        LCM(int _a, int _b): a(_a), b(_b) {}
+        
+        void findLCM(){
+            int res = calcLCM(a,b);
+            std::cout<<"The LCM of the numbers: "<<res<<std::endl;
+        }
+};
+int main() {
+    int a,b;
+    std::cout<<"Enter two numbers: ";
+    std::cin>>a>>b;
+    LCM lcm(a,b);
+    lcm.findLCM();
+    return 0;
+}
+/*
+============================OUTPUT SECTION================================
+INPUT: a = 12, b = 8
+OUTPUT: 24
+----------------------
+INPUT: a = 4, b = 5
+OUTPUT: 20
+-----------------------
+INPUT: a = 1, b = 7
+OUTPUT: 7
+-----------------------
+INPUT: a = 10, b = 10 
+OUTPUT: 10
+-----------------------
+*/
