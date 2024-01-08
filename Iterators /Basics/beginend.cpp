581f40b314cd34a6bbd1c898bcee18c7cf839ba2
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-01-08 15:06:57 +0530

    Create beginend.cpp

---
+++ Iterators /Basics/beginend.cpp
@@ -0,0 +1,26 @@
+/**
+*	  @File: beginend.cpp
+*	  @Author: Nitish Singh
+*	  @Date: January 8, 2024
+*  	@Email: me.singhnitish@yandex.com
+
+*   @Description:
+*   This program demonstrates the use of iterators to iterate through a vector and print its elements to the console.
+**/
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> vec { 1, 2, 3, 4, 5 };
+    std::vector<int>::iterator itr;
+    std::cout << "The vector elements are: ";
+    for (itr = vec.begin(); itr < vec.end(); ++itr) 
+        std::cout << *itr << " ";
+    std::cout << std::endl;
+    system("pause");
+    return 0;
+}
+/**
+The vector elements are: 1 2 3 4 5
+Press any key to continue . . .
+**/
