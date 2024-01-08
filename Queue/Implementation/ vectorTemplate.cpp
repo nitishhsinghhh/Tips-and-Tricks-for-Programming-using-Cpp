274a9c95540dccff3c34db697b149a04836bf7d5
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-01-08 21:50:35 +0530

    Update  vectorTemplate.cpp

--- Queue/Implementation/ vectorTemplate.cpp
+++ Queue/Implementation/ vectorTemplate.cpp
@@ -1,39 +1,40 @@
 #include<iostream>
 #include<vector>
-using namespace std;
 
 template<typename T>
 class Queue {
 private:
-   vector<T> data;
-public: 
-   bool isEmpty(){
-       return data.empty();
-   }
-   
-   void push(T element){
-       data.push_back(element);
-   }
-   
-   void pop(){
-       if(!isEmpty())
-          data.erase(data.begin());
-   }
-   
-   T front(){
-       return data.front();
-   }
+	std::vector<T> data;
+public:
+	bool isEmpty() {
+		return data.empty();
+	}
+
+	void push(T element) {
+		data.push_back(element);
+	}
+
+	void pop() {
+		if (!isEmpty())
+			data.erase(data.begin());
+	}
+
+	T front() {
+		return data.front();
+	}
 };
 
 int main() {
-    Queue<int> queue;
+	Queue<int> queue;
 
-    queue.push(1);
-    queue.push(2);
-    queue.push(3);
+	queue.push(1);
+	queue.push(2);
+	queue.push(3);
 
-    std::cout << "Front: " << queue.front() << std::endl;
+	std::cout << "Front: " << queue.front() << std::endl;
 
-    queue.pop();
-    return 0;
+	queue.pop();
+	return 0;
 }
+
+// Front: 1
