b69cafefdca55b96db0bcf2937250b9ad7f994ab
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-01-08 16:08:23 +0530

    Update advance.cpp

--- Iterators /Basics/advance.cpp
+++ Iterators /Basics/advance.cpp
@@ -13,7 +13,7 @@
 int main() {
 	std::vector<int> vec{ 1, 2, 3, 4, 5 };
 	std::vector<int>::iterator itr = vec.begin();
-	advance(itr, 3);
+	std::advance(itr, 3);
 	std::cout << "The position of the iterator after advancing is: " << *itr << std::endl;
 	system("pause");
 	return 0;
