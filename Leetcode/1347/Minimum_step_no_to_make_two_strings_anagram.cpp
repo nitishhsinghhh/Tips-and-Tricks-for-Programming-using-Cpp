a61f7473f494d3b00e3219e23f33ca11f618b18d
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-17 12:22:25 +0530

    Find the minimum number of steps to make two strings anagram

--- Leetcode/1347/Minimum_step_no_to_make_two_strings_anagram.cpp
+++ Leetcode/1347/Minimum_step_no_to_make_two_strings_anagram.cpp
@@ -9,30 +9,41 @@
 class Solution {
 public:
     int minSteps(std::string s, std::string t) {
+        // Declare two vectors as frequency array to count the number of characters of a string
         std::vector<int>f1(26,0);
         std::vector<int>f2(26,0);
         int i=0;
+        // Count number of characters in string s and put the frequency of character of string s
+        // in the index value of f1
         while(s[i] != 0){
             f1[s[i]-'a']++;
             i++;
         }
         i=0;
+        // Count number of characters in string s and put the frequency of character of string t
+        // in the index value of f2
         while(t[i] != 0){
             f2[t[i]-'a']++;
             i++;
         }
+        // compare two frequncy array if both are same then return 0 as minimum steps number
         if(f1==f2) return 0;
         int res = 0;
+        // count the difference between two strings
         for(int i=0;i<26;i++){
             int diff = abs(f1[i]-f2[i]);
             res += diff;
         }
+        // as we have counted twice (to make one string others' equal) so dividing by 2 is our required 
+        // minimum number of steps
         return res/2;
     }
 };
 
 int main(){
+    // Creating an object of Solution Class
     Solution sol;
+    
     std::string str1, str2;
     int res;
     // test case 1 
