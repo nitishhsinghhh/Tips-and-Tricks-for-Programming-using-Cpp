555ddf6b3a1001c5383df336d91a6411fb01dd3c
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-02-13 15:01:19 +0530

    Create code.cpp

---
+++ ASCII/ASCIIChart/code.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+ 
+int main()
+{
+    std::cout << "Printable ASCII [32..126]:\n";
+    for (char c{' '}; c <= '~'; ++c)
+        std::cout << c << ((c + 1) % 32 ? ' ' : '\n');
+    std::cout << '\n';
+}
