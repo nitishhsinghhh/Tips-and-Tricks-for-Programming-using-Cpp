70ed999811c0617ab29c8685eff2b56b8ac01778
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-03-28 15:17:48 +0530

    Update Sum1.cpp

--- OOP/Lab3/Sum1.cpp
+++ OOP/Lab3/Sum1.cpp
@@ -1,14 +1,14 @@
+
 // Program to display sum of series 1 + 1/2 + 1/3 + 1/4 ... .. .
 
 #include <iostream>
-using namespace std;
 
 class Solution {
 public:
 	float SumofSeries(int num) {
 		float sum = 0;
 		for (int i = 1; i <= num; i++)
-			sum += (float)1/i;
+			sum += (float)1 / i;
 		return sum;
 	}
 };
@@ -17,10 +17,10 @@ int main() {
 	int num;
 	float sum = 0;
 	Solution oSolution;
-	cout << "Enter the value of n: ";
-	cin >> num;
+	std::cout << "Enter the value of n: ";
+	std::cin >> num;
 	float res = oSolution.SumofSeries(num);
-	cout << "Sum is : " << res << endl;
+	std::cout << "Sum is : " << res << std::endl;
 	system("pause");
 	return 0;
 }
