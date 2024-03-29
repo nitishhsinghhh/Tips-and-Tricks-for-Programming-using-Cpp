8985e32cbafb08a1777ba256e34db1472a070091
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-03-29 16:15:46 +0530

    Create DeleteMiddleElement.cpp

---
+++ STL/Containers /Sequence_containers/List/UseCase3/DeleteMiddleElement.cpp
@@ -0,0 +1,98 @@
+#include<iostream>
+
+struct ListNode {
+	int val;
+	ListNode *next;
+	ListNode *prev;
+	ListNode() : val(0), next(nullptr), prev(nullptr) {}
+	ListNode(int x) : val(x), next(nullptr), prev(nullptr) {}
+	ListNode(int x, ListNode *next, ListNode *prev) : val(x), next(next), prev(prev) {}
+};
+
+class Solution {
+private:
+	ListNode* head;
+
+public:
+	Solution() {
+		head = nullptr;
+	}
+
+	// Insert a ListNode at the end of the list
+	void insert(int value) {
+		ListNode* newNode = new ListNode(value);
+		if (head == nullptr)
+			head = newNode;
+		else {
+			ListNode* current = head;
+			while (current->next != nullptr)
+				current = current->next;
+
+			current->next = newNode;
+			newNode->prev = current;
+		}
+	}
+
+	// delete the middle in the list
+	void deleteMiddleelement() {
+		if (head == nullptr) {
+			std::cout << "List is empty " << std::endl;
+			return;
+		}
+
+		ListNode* fast = head, *slow = head;
+		while (fast != nullptr && fast->next != nullptr) {
+			fast = fast->next->next;
+			slow = slow->next;
+		}
+
+		if (slow->prev != nullptr)
+			slow->prev->next = slow->next;
+		else
+			head = slow->next;
+
+		if (slow->next != nullptr)
+			slow->next->prev = slow->prev;
+
+		delete slow;
+	}
+
+	// print the list
+	void printList() {
+		ListNode* current = head;
+		while (current != nullptr) {
+			std::cout << current->val << " ";
+			current = current->next;
+		}
+		std::cout << std::endl;
+	}
+};
+
+int main() {
+	Solution solution;
+
+	// Insert elements into the list
+	solution.insert(1);
+	solution.insert(2);
+	solution.insert(3);
+	solution.insert(4);
+	solution.insert(5);
+
+	// Print the list before deleting the middle element
+	std::cout << "List before deleting middle element: ";
+	solution.printList();
+
+	// Delete the middle element
+	solution.deleteMiddleelement();
+
+	// Print the list after deleting the middle element
+	std::cout << "List after deleting middle element: ";
+	solution.printList();
+
+	return 0;
+}
+
+/**
+List before deleting middle element: 1 2 3 4 5
+List after deleting middle element: 1 2 4 5
+**/
