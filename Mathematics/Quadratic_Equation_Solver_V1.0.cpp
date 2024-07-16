b3b02a7c876f091b4d988977f6e8a779ce5e2e38
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-07-16 09:33:23 +0530

    Update Quadratic_Equation_Solver_V1.0.cpp

--- Mathematics/Quadratic_Equation_Solver_V1.0.cpp
+++ Mathematics/Quadratic_Equation_Solver_V1.0.cpp
@@ -1,34 +1,33 @@
-// Implement a program that solves a quadratic equation (ax^2 + bx + c = 0). 
-// Take coefficients a, b, and c as input.
-#include <bits/stdc++.h>
+// A program that solves a quadratic equation (ax^2 + bx + c = 0).
+// Taking coefficients a, b, and c as input.
+#include <iostream>
 
 int main() {
     double a, b, c;
-    std::cin>>a>>b>>c;
-    //Calculate the value of discriminant
-    double discriminant = b*b - 4*a*c;
-    std::cout<<discriminant<<std::endl;
-    //Check if the discriminant is positive, negative or zero
-    if(discriminant > 0){
-        //Exists two real and distinct roots
-        double root1 = (-b + sqrt(discriminant))/(2*a);
-        double root2 = (-b - sqrt(discriminant))/(2*a);
-        std::cout<<"Root 1: "<<root1<<std::endl;
-        std::cout<<"Root 2: "<<root2<<std::endl;
+    std::cin >> a >> b >> c;
+    // Calculate the value of discriminant
+    double discriminant = b * b - 4 * a * c;
+    std::cout << discriminant << std::endl;
+    // Check if the discriminant is positive, negative or zero
+    if (discriminant > 0) {
+        // Exists two real and distinct roots
+        double root1 = (-b + sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - sqrt(discriminant)) / (2 * a);
+        std::cout << "Root 1: " << root1 << std::endl;
+        std::cout << "Root 2: " << root2 << std::endl;
+    } else if (discriminant == 0) {
+        // Exists one real root
+        double root = (-b) / (2 * a);
+        std::cout << "Root :" << root << std::endl;
+    } else {
+        double realPart = (-b / (2 * a));
+        if (b == 0)
+            realPart = 0;
+        double imaginaryPart = sqrt(-discriminant) / (2 * a);
+        std::cout << "Root 1: " << realPart << " + " << imaginaryPart << "i\n";
+        std::cout << "Root 2: " << realPart << " - " << imaginaryPart << "i\n";
     }
-    else if(discriminant == 0){
-        //Exists one real root
-        double root = (-b)/(2*a);
-        std::cout<<"Root :"<<root<<std::endl;
-    }
-    else{
-        double realPart = (-b/(2*a));
-        if(b == 0) realPart = 0;
-        double imaginaryPart = sqrt(-discriminant)/(2*a);
-        std::cout<<"Root 1: "<<realPart<<" + "<<imaginaryPart<<"i\n";
-        std::cout<<"Root 2: "<<realPart<<" - "<<imaginaryPart<<"i\n";
-    }
-    std::cout<<"Bye\n";
+    std::cout << "Bye\n";
     return 0;
 }
 
@@ -53,14 +52,16 @@ int main() {
 /*
 =======================TOPIC DISCUSSION===================================
   The discriminant is a term used in the context of quadratic equations,
-  and it is part of the quadratic formula. 
-  For a quadratic equation in the form (ax^2 + bx + c = 0), 
-  the discriminant is given by the expression inside the square root of the quadratic formula:
-  
+  and it is part of the quadratic formula.
+  For a quadratic equation in the form (ax^2 + bx + c = 0),
+  the discriminant is given by the expression inside the square root of the
+quadratic formula:
+
   [Discriminant = b^2 - 4ac]
-  
-  The discriminant plays a crucial role in determining the nature of the roots (solutions) of the quadratic equation.
-  Its value provides information about whether the roots are real or complex and whether they are distinct or repeated.
+
+  The discriminant plays a crucial role in determining the nature of the roots
+(solutions) of the quadratic equation. Its value provides information about
+whether the roots are real or complex and whether they are distinct or repeated.
 
   The discriminant can take on three different cases:
 
@@ -72,9 +73,10 @@ int main() {
 
   3. If Discriminant < 0:
    - The roots are complex conjugates (they have both real and imaginary parts).
-     
 
-  The discriminant helps in classifying the solutions and is a fundamental concept when
-  dealing with quadratic equations and their graphical representation. In programming, 
-  it is often used to guide the branching logic in solving quadratic equations.
+
+  The discriminant helps in classifying the solutions and is a fundamental
+concept when dealing with quadratic equations and their graphical
+representation. In programming, it is often used to guide the branching logic in
+solving quadratic equations.
 */
