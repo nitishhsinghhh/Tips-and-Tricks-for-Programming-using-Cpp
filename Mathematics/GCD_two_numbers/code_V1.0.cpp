cadebc0a02add85b03af15f3441f7d9339f8e27d
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-07-16 09:32:03 +0530

    Update code_V1.0.cpp

--- Mathematics/GCD_two_numbers/code_V1.0.cpp
+++ Mathematics/GCD_two_numbers/code_V1.0.cpp
@@ -1,34 +1,40 @@
-// Write a program that calculates the GCD of two numbers. Take the numbers as input.
+// A program that calculates the GCD of two numbers. Take the numbers as input.
 #include <iostream>
-class GCD{
-    private:
-        int n,m;
-        //calculate the GCD of two numbers
-        int calcGCD(int n, int m){
-            if(n==m) return n;
-            if(n==0) return m;
-            else if(m==0) return n;
-            if(n>m) calcGCD(n%m,m);
-            else if(m>n) calcGCD(m%n,n); 
-        }
-    public:
-        //constructor
-        GCD(int a, int b): n(a), m(b) {}
-        void findGCD(){
-            int res = calcGCD(n,m);
-            std::cout<<"The GCD of the numbers: "<<res<<std::endl;
-        }
+class GCD {
+private:
+    int n, m;
+    // calculate the GCD of two numbers
+    int calcGCD(int n, int m) {
+        if (n == m)
+            return n;
+        if (n == 0)
+            return m;
+        else if (m == 0)
+            return n;
+        if (n > m)
+            calcGCD(n % m, m);
+        else if (m > n)
+            calcGCD(m % n, n);
+    }
+
+public:
+    // constructor
+    GCD(int a, int b) : n(a), m(b) {}
+    void findGCD() {
+        int res = calcGCD(n, m);
+        std::cout << "The GCD of the numbers: " << res << std::endl;
+    }
 };
 int main() {
-    int a,b;
-    std::cout<<"Enter two numbers: ";
-    std::cin>>a>>b;
-    GCD gcd(a,b);
+    int a, b;
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+    GCD gcd(a, b);
     gcd.findGCD();
     return 0;
 }
 
-/* 
+/*
 ===================OUTPUT SECTION=============================
 INPUT: a = 5, b = 4
 OUTPUT: 1
