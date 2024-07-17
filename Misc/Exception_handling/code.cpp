d1310e3225a0fc7442a9f5914185fd280f28fb37
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-07-17 15:55:27 +0530

    Update code.cpp

--- Misc/Exception_handling/code.cpp
+++ Misc/Exception_handling/code.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include <stdexcept>
 
-using namespace std;
-
 double divideNumbers(double dividend, double divisor) {
     if (divisor == 0)
-        throw runtime_error("Division by zero error!");
-    
+        throw std::runtime_error("Division by zero error!");
+
     return dividend / divisor;
 }
 
 int main() {
     try {
         double result = divideNumbers(10.0, 0.0);
-        cout << "Result: " << result << endl;
-    } catch (const exception& ex) {
-        cout << "Exception caught: " << ex.what() << endl;
+        std::cout << "Result: " << result << std::endl;
     }
-    
+    catch (const std::exception& ex) {
+        std::cout << "Exception caught: " << ex.what() << std::endl;
+    }
+
     return 0;
 }
