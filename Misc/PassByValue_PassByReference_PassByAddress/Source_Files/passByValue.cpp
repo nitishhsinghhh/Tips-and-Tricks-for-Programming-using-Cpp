a444b35da4cc9645c46f6da216fe1c3a3881cd52
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-12-26 09:54:08 +0530

    Update passByValue.cpp

--- Misc/PassByValue_PassByReference_PassByAddress/Source_Files/passByValue.cpp
+++ Misc/PassByValue_PassByReference_PassByAddress/Source_Files/passByValue.cpp
@@ -1,9 +1,11 @@
 #include "passByValue.h"
 
-int passByValue::smallest_element(const std::vector<int> vec) {
+using std::vector;
+using std::min;
+
+int passByValue::smallest_element(vector<int> vec) {
 	auto smallest_val = vec[0];
 	for (auto x : vec)
-		smallest_val = std::min(smallest_val, x);
-
+		smallest_val = min(smallest_val, x);
 	return smallest_val;
 }
