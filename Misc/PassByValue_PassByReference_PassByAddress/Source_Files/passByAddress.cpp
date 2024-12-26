b8970fda4ca58372c2a7b9cf9d93fc397283385c
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2024-12-26 09:52:05 +0530

    Update passByAddress.cpp

--- Misc/PassByValue_PassByReference_PassByAddress/Source_Files/passByAddress.cpp
+++ Misc/PassByValue_PassByReference_PassByAddress/Source_Files/passByAddress.cpp
@@ -1,10 +1,13 @@
 #include "passByAddress.h"
 
-int passByAddress::smallest_element(const std::vector<int>* vec) {
+using std::vector;
+using std::min;
+
+int passByAddress::smallest_element(vector<int>* vec) {
     if (vec->empty()) return INT_MAX; // Handle empty vector case
     auto smallest_val = (*vec)[0];
     for (auto x : *vec) {
-        smallest_val = std::min(smallest_val, x);
+        smallest_val = min(smallest_val, x);
     }
     return smallest_val;
 }
