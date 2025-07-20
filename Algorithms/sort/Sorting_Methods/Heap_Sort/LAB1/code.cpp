e11042038de48b864f412fb46313d4332f5f1f41
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2025-07-20 20:54:32 +0530

    Update code.cpp

--- Algorithms/sort/Sorting_Methods/Heap_Sort/LAB1/code.cpp
+++ Algorithms/sort/Sorting_Methods/Heap_Sort/LAB1/code.cpp
@@ -1,53 +1,85 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
+/**
+ * @brief Class implementing Heap Sort algorithm.
+ */
 class HeapSort {
 public:
-	static void heapifyDown(vector<int> &nums, int size, int rootInd, bool isMin = false) {
-		if (size <= 1 or rootInd < 0 or rootInd >= size - 1) 
-      return;
-		int keyInd = rootInd, leftChildInd = 2 * rootInd + 1, rightChildInd = 2 * rootInd + 2;
-		if (leftChildInd < size and (isMin ? nums[leftChildInd] < nums[keyInd] : nums[leftChildInd] > nums[keyInd]))
-			keyInd = leftChildInd;
-		if (rightChildInd < size and (isMin ? nums[rightChildInd] < nums[keyInd] : nums[rightChildInd] > nums[keyInd]))
-			keyInd = rightChildInd;
-		if (nums[keyInd] != nums[rootInd]) {
-			swap(nums[rootInd], nums[keyInd]);
-			heapifyDown(nums, size, keyInd, isMin);
-		}
-	}
+    /**
+     * @brief Adjusts the heap downwards from a given root index.
+     * 
+     * @param nums The array representing the heap.
+     * @param size The number of elements in the heap.
+     * @param rootInd The index from where to start heapifying down.
+     * @param isMin Set true for min-heap, false for max-heap (default).
+     */
+    static void heapifyDown(std::vector<int> &nums, int size, int rootInd, bool isMin = false) {
+        if (size <= 1 || rootInd < 0 || rootInd >= size - 1) 
+            return;
 
-	static void heapifyArray(vector<int> &nums, bool isMin = false) {
-		int size = nums.size();
-		if (size <= 1) 
-      return;
-		int tailRootInd = (size >> 1) - 1;
-		for (int rootInd = tailRootInd; rootInd >= 0; rootInd--)
-			heapifyDown(nums, size, rootInd, isMin);
-	}
+        int keyInd = rootInd;
+        int leftChildInd = 2 * rootInd + 1;
+        int rightChildInd = 2 * rootInd + 2;
 
-	static void heapSort(vector<int> &nums) {
-		if (nums.size() <= 1) 
-      return;
-		heapifyArray(nums);
-		for (int size = nums.size() - 1; size; size--) {
-			swap(nums[size], nums[0]);
-			heapifyDown(nums, size, 0);
-		}
-	}
+        if (leftChildInd < size && (isMin ? nums[leftChildInd] < nums[keyInd] : nums[leftChildInd] > nums[keyInd]))
+            keyInd = leftChildInd;
+
+        if (rightChildInd < size && (isMin ? nums[rightChildInd] < nums[keyInd] : nums[rightChildInd] > nums[keyInd]))
+            keyInd = rightChildInd;
+
+        if (nums[keyInd] != nums[rootInd]) {
+            std::swap(nums[rootInd], nums[keyInd]);
+            heapifyDown(nums, size, keyInd, isMin);
+        }
+    }
+
+    /**
+     * @brief Builds a heap from an unsorted array.
+     * 
+     * @param nums The array to be transformed into a heap.
+     * @param isMin Set true to build a min-heap, false for max-heap (default).
+     */
+    static void heapifyArray(std::vector<int> &nums, bool isMin = false) {
+        int size = nums.size();
+        if (size <= 1) 
+            return;
+
+        int tailRootInd = (size >> 1) - 1;
+        for (int rootInd = tailRootInd; rootInd >= 0; rootInd--)
+            heapifyDown(nums, size, rootInd, isMin);
+    }
+
+    /**
+     * @brief Sorts the given array using Heap Sort.
+     * 
+     * @param nums The array to be sorted in ascending order.
+     */
+    static void heapSort(std::vector<int> &nums) {
+        if (nums.size() <= 1) 
+            return;
+
+        heapifyArray(nums);
+        for (int size = nums.size() - 1; size; size--) {
+            std::swap(nums[size], nums[0]);
+            heapifyDown(nums, size, 0);
+        }
+    }
 };
 
+/**
+ * @brief Main function to demonstrate heap sort.
+ * 
+ * @return int Exit status code.
+ */
 int main() {
-	vector<int> nums = { 4, 2, 9, 6, 5, 1, 3, 7, 8 };
-	HeapSort::heapSort(nums);
-	
-	cout << "Heap Sort: { ";
-	for (int num : nums) 
-		cout << num << " ";
-	cout <<"}"<< endl;
+    std::vector<int> nums = { 4, 2, 9, 6, 5, 1, 3, 7, 8 };
+    HeapSort::heapSort(nums);
 
-	system("pause");
-	return 0;
+    std::cout << "Heap Sort: { ";
+    for (int num : nums) 
+        std::cout << num << " ";
+    std::cout << "}" << std::endl;
+
+    return 0;
 }
