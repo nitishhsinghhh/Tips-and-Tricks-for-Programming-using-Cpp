9b99991e3f64790c73fd081a75f270174f8427ce
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2026-03-21 14:29:32 +0530

    Create AsciiComparator.cpp

---
+++ ASCII/CharacterComparison/code/CompareTwoCharacters/AsciiComparator.cpp
@@ -0,0 +1,21 @@
+/**
+ * @class AsciiComparator
+ * @brief Compares two characters and delegates printing.
+ */
+class AsciiComparator : public IComparator {
+private:
+    IPrinter& printer;
+
+public:
+    /**
+     * @brief Constructor with dependency injection.
+     * @param p Reference to a printer object.
+     */
+    AsciiComparator(IPrinter& p) : printer(p) {}
+
+    void compare(char a, char b) override {
+        printer.printAscii(a);
+        printer.printAscii(b);
+        printer.printComparison(a, b);
+    }
+};
