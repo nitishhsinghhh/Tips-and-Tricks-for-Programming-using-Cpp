bba214610b8eda632090a183ce5e36dfd1cbe8f8
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2026-03-21 14:38:00 +0530

    Update AsciiPrinter.cpp

--- ASCII/CharacterComparison/code/CompareTwoCharacters/AsciiPrinter.cpp
+++ ASCII/CharacterComparison/code/CompareTwoCharacters/AsciiPrinter.cpp
@@ -1,3 +1,8 @@
+#include "IPrinter.hpp"
+
+using std::cout; 
+using std::endl;
+
 /**
  * @class AsciiPrinter
  * @brief Prints ASCII values and comparison results.
@@ -5,15 +10,15 @@
 class AsciiPrinter : public IPrinter {
 public:
     void printAscii(char ch) override {
-        std::cout << "ASCII of '" << ch << "' is " << static_cast<int>(ch) << std::endl;
+        cout << "ASCII of '" << ch << "' is " << static_cast<int>(ch) << endl;
     }
 
     void printComparison(char a, char b) override {
         if (a == b)
-            std::cout << "Both characters are equal.\n";
+            cout << "Both characters are equal.\n";
         else if (a < b)
-            std::cout << a << " comes before " << b << " in ASCII order.\n";
+            cout << a << " comes before " << b << " in ASCII order.\n";
         else
-            std::cout << b << " comes before " << a << " in ASCII order.\n";
+            cout << b << " comes before " << a << " in ASCII order.\n";
     }
 };
