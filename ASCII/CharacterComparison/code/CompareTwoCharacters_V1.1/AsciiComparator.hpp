32f02cf81350d3db544745911d6d8de8c5866745
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2026-03-21 16:21:35 +0530

    Update AsciiComparator.hpp

--- ASCII/CharacterComparison/code/CompareTwoCharacters_V1.1/AsciiComparator.hpp
+++ ASCII/CharacterComparison/code/CompareTwoCharacters_V1.1/AsciiComparator.hpp
@@ -1,16 +1,21 @@
 #pragma once
 #include "IComparator.hpp"
+#include "IPrinter.hpp"
 
 /**
  * @class AsciiComparator
  * @brief Compares two characters and delegates printing.
  */
 class AsciiComparator : public IComparator {
+private:
+    IPrinter* printer;
+
 public:
     /**
      * @brief Constructor with dependency injection.
      * @param p Pointer to a printer object.
      */
+    AsciiComparator(IPrinter* p) : printer(p) {}
 
     void compare(char a, char b) override {
         printer->printAscii(a);
