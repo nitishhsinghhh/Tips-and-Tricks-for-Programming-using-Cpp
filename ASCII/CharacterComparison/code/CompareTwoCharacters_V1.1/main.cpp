b238d21583d8f210f71eff121b6e141a6a551ac7
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2026-03-21 15:54:26 +0530

    Update main.cpp

--- ASCII/CharacterComparison/code/CompareTwoCharacters_V1.1/main.cpp
+++ ASCII/CharacterComparison/code/CompareTwoCharacters_V1.1/main.cpp
@@ -5,7 +5,6 @@
  * Nitish Singh
  */
 
-#pragma once
 #include "CharInput.hpp"
 #include "AsciiPrinter.hpp"
 #include "AsciiComparator.hpp"
@@ -15,11 +14,9 @@ int main() {
     char ch1 = input.getChar("first");
     char ch2 = input.getChar("second");
 
-    IPrinter* printer = new AsciiPrinter();
-    IComparator* comparator = new AsciiComparator(printer);
-    comparator->compare(ch1, ch2);
+    AsciiPrinter printer;
+    AsciiComparator comparator(&printer);
+    comparator.compare(ch1, ch2);
 
-    delete comparator;
-    delete printer;
     return 0;
 }
