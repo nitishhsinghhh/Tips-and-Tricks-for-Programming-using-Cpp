32033c35bfb5aea2d44064c4865f3917b4df8b38
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2026-03-21 17:01:04 +0530

    Update main.cpp

--- ASCII/CharacterComparison/code/CompareTwoCharacters_V1.2/main.cpp
+++ ASCII/CharacterComparison/code/CompareTwoCharacters_V1.2/main.cpp
@@ -1,13 +1,15 @@
-/**
- * @file CharAsciiCompareSOLID.cpp
- * @brief Compares two characters based on ASCII values using SOLID principles.
- * @author 
- * Nitish Singh
- */
-
 #include "CharInput.hpp"
 #include "AsciiPrinter.hpp"
 #include "AsciiComparator.hpp"
+
+/**
+ * @brief Compare two characters using a printer.
+ */
+void compareChars(char ch1, char ch2, IPrinter* printer) {
+    IComparator* comparator = new AsciiComparator(*printer);
+    comparator->compare(ch1, ch2);
+    delete comparator;
+}
 
 int main() {
     CharInput input;
@@ -15,10 +17,8 @@ int main() {
     char ch2 = input.getChar("second");
 
     IPrinter* printer = new AsciiPrinter();
-    IComparator* comparator = new AsciiComparator(printer);
-    comparator->compare(ch1, ch2);
-
-    delete comparator;
+    compareChars(ch1, ch2, printer);
     delete printer;
+
     return 0;
 }
