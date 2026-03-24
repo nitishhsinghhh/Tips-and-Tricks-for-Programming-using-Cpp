4982bb190b5a920689d3c833d58fd8753cd7b935
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2026-03-24 13:48:57 +0530

    New Test data

--- Leetcode/3497/setup.sql
+++ Leetcode/3497/setup.sql
@@ -14,7 +14,19 @@ INSERT INTO UserActivity (user_id, activity_type, activity_duration) VALUES
 (1, 'free_trial', 30),
 (1, 'paid', 60),
 (1, 'paid', 90),
-(2, 'free_trial', 20),
-(2, 'paid', 40),
-(3, 'free_trial', 15);
+(1, 'paid', 75),
+(2, 'free_trial', 55),
+(2, 'free_trial', 25),
+(2, 'free_trial', 50),
+(2, 'cancelled', 0),
+(3, 'free_trial', 70),
+(3, 'free_trial', 60),
+(3, 'free_trial', 80),
+(3, 'paid', 50),
+(3, 'paid', 55),
+(3, 'paid', 85),
+(4, 'free_trial', 40),
+(4, 'free_trial', 35),
+(4, 'paid', 45),
+(4, 'cancelled', 0);
 GO
