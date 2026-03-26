0b31fa8787853e266d624af75639f9992024efdb
Nitish Singh <93253740+nitishhsinghhh@users.noreply.github.com>
2026-03-26 12:49:14 +0530

    Add gitignore

--- Working_Application/String_Conversion/build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nitishsingh/Documents/GitHub/Tips-and-Tricks-for-Programming-using-Cpp/Repo/Working_Application/String_Conversion/build/_deps/googletest-src
-# Build directory: /Users/nitishsingh/Documents/GitHub/Tips-and-Tricks-for-Programming-using-Cpp/Repo/Working_Application/String_Conversion/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
